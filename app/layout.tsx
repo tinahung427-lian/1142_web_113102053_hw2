6ec849ffd5a5dd96a7c490926c40b45596fa2872
tinahung427-lian <tinahung427@gmail.com>
2026-05-28 00:55:33 +0800

    color

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,10 +27,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="h-full flex justify-center w-full bg-[#ffffff] pt-4 pb-4">
+      <body className="h-full flex justify-center w-full bg-[#fcf5e9] pt-4 pb-4">
         
         
-        <div className=" bg-[#deeefb] max-w-[480px] w-full h-full rounded-2xl p-4">
+        <div className=" bg-[#b2d3e0] max-w-[480px] w-full h-full rounded-2xl p-4">
         {children}
         </div>
         
--- app/question/page.tsx
+++ app/question/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import Link from "next/link";
-import { useState, useEffect  } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { usePsyStore } from "../../store/store";
 
@@ -9,87 +8,81 @@ export default function Question() {
   const router = useRouter();
   const [questionIndex, setQuestionIndex] = useState(0);
 
-  const psyData = usePsyStore( (state) => state.psyData);
+  const psyData = usePsyStore((state) => state.psyData);
   const setScore = usePsyStore((state) => state.setScore);
 
-  console.log(psyData);
-  console.log(psyData.quizData);
-
-
-  useEffect( () => {
+  useEffect(() => {
     console.log("目前分數:" + psyData.score);
   }, [psyData.score]);
-  
 
-  function nextQuestion(optionIndex: any) {
+  function nextQuestion(optionIndex: number) {
     console.log("使用者選擇：" + optionIndex);
 
-    setScore( psyData.score + psyData.quizData[questionIndex].options[optionIndex].value );
-    console.log( psyData.score );
+    setScore(
+      psyData.score +
+        psyData.quizData[questionIndex].options[optionIndex].value
+    );
 
-
-    if( questionIndex != psyData.quizData.length-1 ) {
-      console.log("下一題");
+    if (questionIndex != psyData.quizData.length - 1) {
       setQuestionIndex(questionIndex + 1);
     } else {
-      console.log("進入準備看結果頁面");
       router.push("/prepare");
     }
   }
 
- 
   return (
     <>
+      <div className="w-full max-w-[420px] px-6 py-10 mx-auto">
 
-<div className="w-full max-w-[420px] px-6 py-10 mx-auto">
+        {/* 題目 */}
+        <div className="flex flex-col items-center mb-10">
 
-{/* 題目 */}
-<div className="flex items-center gap-4 mb-10">
+          <div className="w-12 h-12 rounded-full bg-[#c86b4a] flex items-center justify-center text-[#fff8ef] text-2xl font-black mb-3">
+            Q{questionIndex + 1}
+          </div>
 
-  <div className="text-[#000000] text-3xl font-black">
-    Q{questionIndex + 1}
-  </div>
+          <div className="w-fit">
+            <div className="text-[#2f2a26] text-xl font-black leading-snug text-left">
+              {psyData.quizData[questionIndex].title}
+            </div>
+          </div>
 
-  <div className="text-black text-2xl font-black leading-snug">
-    {psyData.quizData[questionIndex].title}
-  </div>
+        </div>
 
-</div>
+        {/* 選項 */}
+        <div className="flex flex-col gap-5">
+          {psyData.quizData[questionIndex].options.map(
+            (option: any, index: number) => {
+              return (
+                <button
+                  key={index}
+                  onClick={() => nextQuestion(index)}
+                  className="
+                    w-full
+                    min-h-[60px]
+                    bg-[#c86b4a]
+                    hover:bg-[#b85c3c]
+                    rounded-[20px]
+                    border-none
+                    outline-none
+                    px-6
+                    py-2
+                    text-[#fff8ef]
+                    text-base
+                    font-bold
+                    leading-relaxed
+                    hover:-translate-y-1
+                    transition
+                  "
+                >
+                  {option.text}
+                </button>
+              );
+            }
+          )}
+        </div>
 
-{/* 選項 */}
-<div className="flex flex-col gap-5">
-  {
-    psyData.quizData[questionIndex].options.map(
-      (option: any, index: number) => {
-        return (
-          <button
-            key={index}
-            onClick={() => nextQuestion(index)}
-            className="
-              w-full
-              min-h-[90px]
-              bg-[#2c272b]
-              border-2
-              rounded-[20px]
-              px-6
-              py-4
-              text-white
-              text-lg
-              font-bold
-              leading-relaxed
-              hover:translate-y-[-3px]
-              transition
-            "
-          >
-            {option.text}
-          </button>
-        )
-      }
-    )
-  }
-</div>
-
-</div>
+      </div>
     </>
   );
 }
